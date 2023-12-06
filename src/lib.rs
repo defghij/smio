d0d588bc1eaa1b5f82d07de9f9b90f03c18abdb8
defghij <defghij@arda>
2023-12-06 16:55:54 -0500

    Modified AioContext, some tests, and struggled with a Quill type

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use scribe::page;
+use quill::{Quill, QuillKind};
+
+mod integration_tests {
+    use super::{Quill, QuillKind};
+
+    #[test]
+    fn use_aio_to_write_pages() {
+        let quil: Quill = Quill::new(QuillKind::AIO);
+
+
+    }
+
+}
